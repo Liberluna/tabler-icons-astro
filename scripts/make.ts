f6d8ce7f22418fd7eb4b942d0c699ebbd2c5e6f3
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-03 19:07:31 +0900

    fix

--- scripts/make.ts
+++ scripts/make.ts
@@ -1,13 +1,13 @@
 import * as icons from "https://esm.sh/@tabler/icons@2.21.0"
 
-await Deno.mkdir("./dist/icons", { recursive: true })
+await Deno.mkdir("./src/icons", { recursive: true })
 
 for(const [name, svg] of Object.entries(icons)){
-  const filename = `./dist/icons/${name}.astro`
+  const filename = `./src/icons/${name}.astro`
   const body = `${svg}`
   await Deno.writeTextFile(filename, body)
 }
 
 const entryCode = Object.keys(icons).map(name => `export { default as ${name} } from "./icons/${name}.astro";`).join("\n")
 
-await Deno.writeTextFile("./dist/index.js", entryCode)
+await Deno.writeTextFile("./src/index.js", entryCode)
