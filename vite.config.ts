b8fd34d2b840472067cc7e10994f68614b54d47c
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-03 19:08:16 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     lib: {
-      entry: './lib/main.ts',
-      name: 'Counter',
-      fileName: 'counter'
+      entry: './src/index.js',
+      name: 'tablerIconsAstro',
+      fileName: 'index'
     }
   }
 })
